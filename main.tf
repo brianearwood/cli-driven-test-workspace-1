08364390254c47690b504f166ffe113b46e9313f
Brian Earwood <brianearwood@gmail.com>
2025-10-14 11:11:26 -0500

    add doormat provider and aws creds

--- main.tf
+++ main.tf
@@ -5,12 +5,28 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    doormat = {
+      source  = "doormat.hashicorp.services/hashicorp-security/doormat"
+      version = "~> 0.0.2"
+    }
   }
+}
+
+# Configure the Doormat Provider
+provider "doormat" {}
+
+data "doormat_aws_credentials" "creds" {
+  provider = doormat
+
+  role_arn = "arn:aws:iam::886363944443:role/sample_dev-custom_role"
 }
 
 # Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+  region     = "us-east-1"
+  access_key = data.doormat_aws_credentials.creds.access_key
+  secret_key = data.doormat_aws_credentials.creds.secret_key
+  token      = data.doormat_aws_credentials.creds.token
 }
 
 # Create S3 bucket
